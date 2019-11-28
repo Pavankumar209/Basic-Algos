365faf79c9edc840e1842119b8cd060a796125cb
Pavankumar209 <45729258+Pavankumar209@users.noreply.github.com>
2019-11-28 12:27:37 +0530

    Create linear_search_basic.java

---
+++ Strings/linear_search_basic.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String hackerrankInString(String s) {
+        String h="hackerrank";
+        int p1=0,p2=0;
+        int m=h.length();
+        int n=s.length();
+        while(p1<m && p2<n){
+            if(h.charAt(p1)==s.charAt(p2)){
+                p1++;
+                p2++;
+            }
+            else{
+                p2++;
+            }
+        }
+        if(p1==m){
+            return "YES";
+        }
+        else if(p2==n){
+            return "NO";
+        }
+        return "-1";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            String result = hackerrankInString(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
