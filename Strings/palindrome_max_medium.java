7051b0074ba48d90d2dcc30e865cf1235b0667ac
Pavankumar209 <45729258+Pavankumar209@users.noreply.github.com>
2019-11-29 20:56:13 +0530

    Create palindrome_max_medium.java

---
+++ Strings/palindrome_max_medium.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String highestValuePalindrome(String s, int n, int k) {
+        BigInteger b = new BigInteger(s);
+        char[] c=s.toCharArray();
+        int u=0;
+        for(int i=0;i<n/2;i++){
+            if(s.charAt(i)!=s.charAt(n-i-1))
+                u++;
+        }
+        if(k<u){
+            System.out.println("K "+k+" U"+u);
+            return "-1";
+        }
+        if(k==u){
+            for(int i=0;i<n/2;i++){
+                if(s.charAt(i)!=s.charAt(n-i-1)){
+                    if(s.charAt(i)>s.charAt(n-i-1)){
+                        c[n-i-1]=c[i];
+                    }
+                    else{
+                        c[i]=c[n-i-1];
+                    }
+                }
+            }
+            String s1=new String(c);
+            return s1;
+        }
+        if(k>u){
+            int x=k-u;
+            k=k-x;
+            int eo=x%2;
+            if(x>1){
+                for(int i=0;i<=n/2;i++){
+                    if((s.charAt(i)==s.charAt(n-i-1))&&(s.charAt(i)!='9')){
+                        c[n-i-1]='9';
+                        c[i]='9';
+                        x=x-2;
+                        if(i==n/2+1){
+                            System.out.println("Came here");
+                            x++;
+                        }
+                    }
+                    if(x<=1)
+                        break;
+                }
+                s=new String(c);
+            }
+            if(x==1){
+                for(int i=0;i<=n/2;i++){
+                    System.out.println("ooo");
+                    if((s.charAt(i)!=s.charAt(n-i-1))&&(s.charAt(i)!='9' && s.charAt(n-i-1)!='9')){
+                        System.out.println("ooolaala");
+                        c[n-i-1]='9';
+                        c[i]='9';
+                        x=x-1;
+                    }
+                    if(i==n/2 && n%2==1 && x==1){
+                        System.out.println("Came here2");
+                        x--;
+                        c[i]='9';
+                    }
+                    if(x<1){
+                        System.out.println("oooend");
+                        break;
+                    }
+                }
+                s=new String(c);
+            }
+            if(true){
+                for(int i=0;i<=n/2;i++){
+                    if(s.charAt(i)!=s.charAt(n-i-1)){
+                        if(s.charAt(i)>s.charAt(n-i-1)){
+                            c[n-i-1]=c[i];
+                        }
+                        else{
+                            c[i]=c[n-i-1];
+                        }
+                        k--;
+                        if(k==0){
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        String s1=new String(c);
+        return s1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        String s = scanner.nextLine();
+
+        String result = highestValuePalindrome(s, n, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
